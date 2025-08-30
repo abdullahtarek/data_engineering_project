898a4cd4d4646f272b2b5134e948ef0b9c2154d4
Abdullah Tarek <abdullahtarek@rocketmail.com>
2025-08-30 10:55:55 +0200

    add_dbt_env

--- data-pipelines/dags/number_of_users_dag.py
+++ data-pipelines/dags/number_of_users_dag.py
@@ -2,8 +2,10 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators import BashOperator
 
-def hello_world():
-    print("Hello World")
+import sys
+import os
+import pathlib
+folder_path = pathlib.Path(__file__).parent.resolve()
 
 default_args = {
     'owner': 'airflow',
@@ -11,19 +13,40 @@ default_args = {
     'retries': 0,
 }
 
+python_env = "/home/dbtEnv"
+requirements_file = os.path.join(folder_path,"../dbt_requirements.txt")
+
 with DAG(
     dag_id='number_of_users_dag',
     default_args=default_args,
-    description='A simple DAG that prints Hello World every 5 minutes',
+    description='A DAG to run DBT model number_of_users',
     schedule_interval='*/5 * * * *',  # every 5 minutes
     start_date=datetime(2025, 1, 1),  # change to your desired start date
     catchup=False,
-    tags=['example'],
 ) as dag:
+
+    # Environment setup operator
+    setup_environment = BashOperator(
+        task_id="setup_environment",
+        bash_command=f"""
+        if [ -d "{python_env}" ]; then
+            echo "Virtual environment already exists at {python_env}, Deleting old env"
+            rm -r {python_env}
+        fi
+
+        echo "Creating virtual environment at {python_env}"
+        python3 -m venv {python_env}
+
+        echo "Installing requirements from {requirements_file}"
+        {python_env}/bin/pip install -r {requirements_file}
+        """,
+        dag=dag,
+    )
+
 
     task1 = BashOperator(
         task_id='number_of_users',
         python_callable='echo "Hello world" ',
     )
 
-    task1
+    setup_environment >> task1
